d6c76fbc4a4149f02c440284f69b80f400341df7
Crowdbotics <team@crowdbotics.com>
2024-03-08 15:06:16 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ninjatech-demo-01-46882",
+  url: "https://ninjatech-demo-01-46882.botics.co",
+  api: "https://ninjatech-demo-01-46882.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
